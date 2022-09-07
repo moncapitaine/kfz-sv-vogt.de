d6437f53520bb2a949666c88b6043246547ad602
Michael Vogt <michael.vogt@progwise.net>
2022-09-07 15:17:01 +0200

    removed phone number

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -9,6 +9,5 @@
 		<h2 class="text-xl">Max Vogt</h2>
 		<h2 class="text-xl">Attachinger Weg 37</h2>
 		<h2 class="text-xl">85356 Freising</h2>
-		<h2 class="text-xl">Telefon +49 176 24433906</h2>
 	</article>
 </section>
